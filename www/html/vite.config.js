7ea5e0f946015502b077621f8ae8e60877e81353
Mizuno Shota <shota.mizuno@epkotsoftware.co.jp>
2026-04-17 10:27:45 +0900

    readme修正、vite.config.jsを本番環境でも動作するように修正

--- www/html/vite.config.js
+++ www/html/vite.config.js
@@ -5,7 +5,20 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.jsx', 'resources/css/app.css'],
+            input: [
+                'resources/js/app.jsx',
+                'resources/css/app.css',
+                'resources/js/Pages/Welcome.jsx',
+                'resources/js/Pages/Dashboard.jsx',
+                'resources/js/Pages/Map.jsx',
+                'resources/js/Pages/Auth/Login.jsx',
+                'resources/js/Pages/Auth/Register.jsx',
+                'resources/js/Pages/Auth/ConfirmPassword.jsx',
+                'resources/js/Pages/Auth/ForgotPassword.jsx',
+                'resources/js/Pages/Auth/ResetPassword.jsx',
+                'resources/js/Pages/Auth/VerifyEmail.jsx',
+                'resources/js/Pages/Profile/Edit.jsx',
+            ],
             refresh: true,
         }),
         react(),
